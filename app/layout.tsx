2873b74569a7c8765d29040eddc31a70636c8e25
bala5358 <balainfilabs@gmail.com>
2024-06-29 17:20:12 +0530

    update files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import "../styles/globals.css";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Liftoff - AI-Powered Mock Interviews",
+  title: "Valo Team",
   openGraph: {
-    title: "Liftoff - AI-Powered Mock Interviews",
+    title: "Valo Team",
     description:
       "Liftoff is an AI-powered mock interview platform that helps you practice for your next job interview.",
     images: [
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Liftoff - AI-Powered Mock Interviews",
+    title: "Valo Team",
     description:
       "Liftoff is an AI-powered mock interview platform that helps you practice for your next job interview.",
     images: ["https://demo.useliftoff.com/opengraph-image"],
